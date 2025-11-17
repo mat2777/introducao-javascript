5142493177512ed81930eeeac176e1772e8d59bd
mat <matheus.melo.araujo@escola.pr.gov.br>
2025-11-17 09:58:41 -0300

    Organização de código e organizando funções

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -45,35 +45,26 @@ for (var i=0; i < pacientes.length; i++) {
 
 }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
+// Reescrever o mesmo código em três funções independentes
+function calculaimc(peso, altura){
+    var imc = 0;
+    imc = peso/(altura*altura);
+    return imc.toFixed(2);
 
-botaoAdicionar.addEventListener("click", function(){
-    
-    var form = document.querySelector("#form-adiciona");
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+}
 
-    var pacienteTr = document.createElement("tr");
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
+function validaPeso(peso){
+    if (peso <= 0 || peso >= 1000) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
-});
+function validaAltura(altura){
+    if (altura <=0 || altura > 3.00){
+        return false;
+    } else {
+        return true;
+    }
+}
