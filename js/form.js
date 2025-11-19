355cfaa7fcae1d5fefa461dea710ce15499bb298
mat <matheus.melo.araujo@escola.pr.gov.br>
2025-11-19 11:41:18 -0300

    erro de digitação

--- js/form.js
+++ js/form.js
@@ -24,7 +24,7 @@ function obtemPacienteDoFormulario(form){
         peso: form.peso.value,
         altura: form.altura.value,
         gordura: form.gordura.value,
-        imc: calcularImc(form.peso.value, form.altura.value)
+        imc: calculaImc(form.peso.value, form.altura.value)
     }
     return paciente;
 }
